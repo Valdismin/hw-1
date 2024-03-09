0d04068da9f859076096314f1493cc1d85a636a1
Vladislav Kozlovski <Valdismin@gmail.com>
2024-03-09 22:11:55 +0100

    add blogs

--- src/validation/blogs.ts
+++ src/validation/blogs.ts
@@ -1,17 +1,17 @@
 import {body} from "express-validator";
 
-const blogNameValidation = body('name').trim().isString().isLength({min: 1})
-const blogDescriptionValidation = body('description').trim().isString().notEmpty()
-const blogURLValidation = body('websiteUrl').trim().isString().notEmpty()
+const blogNameValidation = body('name').trim().isString().bail().isLength({min: 1}).bail()
+const blogDescriptionValidation = body('description').trim().isString().bail().notEmpty().bail()
+const blogURLValidation = body('websiteUrl').trim().isString().bail().notEmpty().bail()
 export const createBlogValidation = [
     blogNameValidation,
     blogDescriptionValidation,
     blogURLValidation
 ]
 
-const blogNameUpdateValidation = body('name').trim().isString().isLength({min: 1, max:15})
-const blogDescriptionUpdateValidation = body('description').trim().isString().isLength({min: 1, max:500})
-const blogURLUpdateValidation = body('websiteUrl').trim().isString().isLength({min: 1, max:100}).matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/)
+const blogNameUpdateValidation = body('name').trim().isString().bail().isLength({min: 1, max:15}).bail()
+const blogDescriptionUpdateValidation = body('description').trim().isString().bail().isLength({min: 1, max:500}).bail()
+const blogURLUpdateValidation = body('websiteUrl').trim().isString().bail().isLength({min: 1, max:100}).bail().matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/).bail()
 
 export const updateBlogValidation = [
     blogNameUpdateValidation,
