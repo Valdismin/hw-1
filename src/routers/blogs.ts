432ac97c1915c66c87676d35a0d279a6637c474b
Vladislav Kozlovski <Valdismin@gmail.com>
2024-03-09 22:30:02 +0100

    add auth middleware

--- src/routers/blogs.ts
+++ src/routers/blogs.ts
@@ -5,11 +5,12 @@ import {
 } from "../controllers/blogsController";
 import {createBlogValidation, updateBlogValidation} from "../validation/blogs";
 import {inputCheckErrorsMiddleware} from "../middlewares/createErrorMiddleware";
+import {authMiddleware} from "../middlewares/auth";
 
 export const blogsRouter = Router()
 
 blogsRouter.get('/', getBlogsController)
-blogsRouter.post('/', createBlogValidation, inputCheckErrorsMiddleware, createBlogController)
+blogsRouter.post('/', [authMiddleware, ...createBlogValidation], inputCheckErrorsMiddleware, createBlogController)
 blogsRouter.get('/:id', findBlogController)
-blogsRouter.put('/:id', updateBlogValidation, inputCheckErrorsMiddleware, updateBlogController)
-blogsRouter.delete('/:id', deleteBlogController)
+blogsRouter.put('/:id', [authMiddleware, ...updateBlogValidation], inputCheckErrorsMiddleware, updateBlogController)
+blogsRouter.delete('/:id', authMiddleware, deleteBlogController)
