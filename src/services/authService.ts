08d97c6dda7c42d583b9002e2b0834db9d29efe0
Vladislav Kozlovski <Valdismin@gmail.com>
2024-03-29 19:44:44 +0100

    4hw

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -2,8 +2,8 @@ import bcrypt from "bcrypt";
 import {authRepository} from "../repositories/authRepository";
 
 export const authService = {
-    authUser: async (user, password) => {
-        await authRepository.login(user.loginOrEmail)
+    authUser: async (loginOrEmail, password) => {
+        const user = await authRepository.login(loginOrEmail)
         const hashedPassword = await bcrypt.hash(password, user?.salt!)
         return hashedPassword === user?.hash;
     }
