fc3eab0b3daf391997ec64bf256bd147bffad53f
Vladislav Kozlovski <Valdismin@gmail.com>
2024-04-08 22:33:57 +0200

    6hw

--- src/middlewares/checkJWT.ts
+++ src/middlewares/checkJWT.ts
@@ -10,6 +10,10 @@ export const checkJWT = (req: Request, res: Response, next: NextFunction) => {
 
     const token = req.headers.authorization.split(' ')[1]
     const userId = JWTService.getUserIdByToken(token)
+    if (!userId) {
+        res.status(401).end()
+        return
+    }
     req.userId = userId
     next()
 }
