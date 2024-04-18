aeff1a1dbd0239bf36fb7ef9c52a4bd391d40604
Vladislav Kozlovski <Valdismin@gmail.com>
2024-04-19 00:03:22 +0200

    7hw

--- src/managers/emailManager.ts
+++ src/managers/emailManager.ts
@@ -1,7 +1,7 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-    service: 'yandex',
+    service: 'gmail',
     auth: {
         user: process.env.GMAIL_LOGIN || '',
         pass: process.env.GMAIL_PASSWORD || ''
