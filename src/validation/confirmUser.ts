1843e3332cb452c0da1a8c04bd0dc5fcda2c8dfc
Vladislav Kozlovski <Valdismin@gmail.com>
2024-04-18 23:38:10 +0200

    7hw

--- src/validation/confirmUser.ts
+++ src/validation/confirmUser.ts
@@ -3,20 +3,23 @@ import {usersQueryRepository} from "../repositories/usersQueryRepository";
 
 export const confirmUserValidation = body('code').trim().isString().custom(async (value) => {
     const user = await usersQueryRepository.getUserByConfirmCode(value)
-    if(user?.userConfirmation.confirmed){
+    if (!user) {
+        throw new Error('User not found');
+    }
+    if(user.userConfirmation.confirmed){
         throw new Error('User already confirmed');
     }
-    if(user?.userConfirmation.expirationTime < new Date()){
+    if(user.userConfirmation.expirationTime < new Date()){
         throw new Error('Code expired');
     }
-    if(user?.userConfirmation.confirmCode !== value){
+    if(user.userConfirmation.confirmCode !== value){
         throw new Error('Invalid code');
     }
 })
 
 export const resendEmailValidation = body('email').trim().matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/).bail().custom(async (value) => {
     const user = await usersQueryRepository.getUserForAuth(value)
-    if(user?.userConfirmation.confirmed){
+    if(user?.userConfirmation?.confirmed){
         throw new Error('User already confirmed');
     }
 })
