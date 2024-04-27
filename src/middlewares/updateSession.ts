80652fd82648109b9263520264536decfc08ccba
Vladislav Kozlovski <Valdismin@gmail.com>
2024-04-28 00:23:34 +0200

    hw9

--- src/middlewares/updateSession.ts
+++ src/middlewares/updateSession.ts
@@ -1,12 +1,14 @@
 import {NextFunction, Request, Response} from "express";
-import {securityRepository} from "../repositories/securityRepository";
 import {JWTService} from "../services/JWTService";
 import {securityService} from "../services/securityService";
 
 export const updateSession = async (req: Request, res: Response, next: NextFunction) => {
     const refreshToken = req.cookies.refreshToken
+    if (!refreshToken) {
+        next()
+        return
+    }
     const tokenFields = JWTService.getFieldsForDeviceSession(refreshToken)
 
     await securityService.updateLastActiveDate(tokenFields.userId, tokenFields.deviceId)
-    next()
 }
