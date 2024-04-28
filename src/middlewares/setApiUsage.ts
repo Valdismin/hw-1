3135101368d0e4fe7311c1e1a0a217d3c2335575
Vladislav Kozlovski <Valdismin@gmail.com>
2024-04-28 18:36:03 +0200

    hw9

--- src/middlewares/setApiUsage.ts
+++ src/middlewares/setApiUsage.ts
@@ -3,7 +3,7 @@ import {apiUsageService} from "../services/apiUsageService";
 
 export const setApiUsage = async (req: Request, res: Response, next: NextFunction) => {
     const IP = req.ip;
-    const URL = req.baseUrl || req.originalUrl
+    const URL = req.originalUrl || req.baseUrl
     await apiUsageService.setRecord(IP!, URL);
     next();
 }
